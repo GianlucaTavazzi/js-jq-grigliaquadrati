c05b8fcf521e4a45c8dcbccd50871df731af447f
Gianluca Tavazzi <gianluca.tavazzi@gmail.com>
2020-05-07 16:36:44 +0200

    fine

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 //seleziono la Griglia e al suo interno metto 25 quadrati
 
 for (var i = 0; i < 25; i++) {
-    $('#griglia').append('<div class="quadrato"><p class="numero"></p></div>')
-    $('p').each(function(i){
-        var numero_casuale = numero_random(0, 10);
-
-        $(this).html(numero_casuale);
-    })
+    $('#griglia').append('<div class="quadrato"><p></p></div>')
 }
 
 $('p').each(function(i){
-    var numero_p = $(this).text()
-    console.log(numero_p);
+    var numero_casuale = numero_random(0, 10);
+
+    $(this).html(numero_casuale);
+    console.log(numero_casuale);
+
+    if (numero_casuale == 0) {
+        $(this).addClass('verde')
+    } else if (numero_casuale%2 == 0){
+        $(this).addClass('rosso')
+    }
 })
 
 
